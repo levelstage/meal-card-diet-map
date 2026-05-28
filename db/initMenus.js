1c5cbabc7476bbe4cfe87d0afb3771fc1c3d7d0c
levelstage <chsu2040@yonsei.ac.kr>
2026-05-29 08:13:30 +0900

    fix: 오타수정2

--- db/initMenus.js
+++ db/initMenus.js
@@ -1,11 +1,11 @@
 const Database = require('better-sqlite3');
-const db = new Database('stores.db');
+const db = new Database('menus.db');
 
 db.exec(`
     CREATE TABLE IF NOT EXISTS menus (
     storeId   TEXT,
-    menuName TEXT PRIMARY KEY,
-    energy INT, 
+    menuName  TEXT,
+    energy INT,
     carb INT,
     prot INT,
     fat INT,
